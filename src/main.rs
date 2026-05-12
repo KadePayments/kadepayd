5304952224382e1dbe4bc59716f509c8d04c3e9f
Shubert Munthali <shubertm.m@gmail.com>
2026-05-13 00:03:58 +0200

    Invoice proto for gRPC

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ use crate::server::engine::Engine;
 
 mod server;
 
+pub mod invoice {
+    tonic::include_proto!("kadepay.v1.services.invoice");
+}
+
 #[tokio::main]
 async fn main() {
     let server_config = Config::new();
-    Engine::start(server_config.server_url ,|_| async {
+    Engine::start(server_config.server_url, |_| async {
         println!("Engine started!");
-    }).await;
+    })
+    .await;
 }
