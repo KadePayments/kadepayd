997be3ce23dedd7c3842618afa25e2b424c7cdf5
Shubert Munthali <shubertm.m@gmail.com>
2026-05-19 23:06:59 +0200

    Match env and .secret variable keys

--- src/server/config.rs
+++ src/server/config.rs
@@ -13,26 +13,34 @@ pub struct Config {
 }
 
 impl Config {
+    const KADEPAY_HOST_KEY: &'static str = "KADEPAY_HOST";
+    const KADEPAY_INVOICES_PORT_KEY: &'static str = "KADEPAY_INVOICES_PORT";
+    const KADEPAY_DB_HOST_KEY: &'static str = "KADEPAY_DB_HOST";
+    const KADEPAY_DB_URL_KEY: &'static str = "KADEPAY_DB_URL";
+    const KADEPAY_DB_USER_KEY: &'static str = "KADEPAY_DB_USER";
+    const KADEPAY_DB_PASSWORD_KEY: &'static str = "KADEPAY_DB_PASSWORD";
+    const KADEPAY_DB_NAME_KEY: &'static str = "KADEPAY_DB_NAME";
+
     pub fn new() -> Config {
         let local_secrets = read_local_secrets();
 
-        let host = env::var("KADEPAY_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-        let port = env::var("KADEPAY_INVOICES_PORT")
+        let host = env::var(Self::KADEPAY_HOST_KEY).unwrap_or_else(|_| "0.0.0.0".to_string());
+        let port = env::var(Self::KADEPAY_INVOICES_PORT_KEY)
             .ok()
-            .or_else(|| local_secrets.get("kadepay_invoices_port").cloned())
-            .expect("Missing KADEPAY_INVOICES_PORT environment variable or kadepay_invoices_port in secrets");
+            .or_else(|| local_secrets.get(Self::KADEPAY_INVOICES_PORT_KEY).cloned())
+            .expect("Missing KADEPAY_INVOICES_PORT in environment variables or secrets");
         let server_url = format!("{}:{}", host, port);
         let kadepay_invoices_server_addr = match server_url.parse::<SocketAddr>() {
             Ok(addr) => addr,
             Err(_) => panic!("Invalid server url: {}", server_url),
         };
 
-        let db_host_option = env::var("KADEPAY_DB_HOST")
+        let db_host_option = env::var(Self::KADEPAY_DB_HOST_KEY)
             .ok()
-            .or_else(|| local_secrets.get("kadepay_db_host").cloned());
-        let db_url_option = env::var("KADEPAY_DB_URL")
+            .or_else(|| local_secrets.get(Self::KADEPAY_DB_HOST_KEY).cloned());
+        let db_url_option = env::var(Self::KADEPAY_DB_URL_KEY)
             .ok()
-            .or_else(|| local_secrets.get("kadepay_db_url").cloned());
+            .or_else(|| local_secrets.get(Self::KADEPAY_DB_URL_KEY).cloned());
 
         if db_host_option.is_none() && db_url_option.is_none() {
             panic!("Database host or url must be set");
@@ -41,18 +49,18 @@ impl Config {
         let db_host = db_host_option.unwrap_or_default();
         let db_url = db_url_option.unwrap_or_default();
 
-        let db_user = env::var("KADEPAY_DB_USER")
+        let db_user = env::var(Self::KADEPAY_DB_USER_KEY)
             .ok()
-            .or_else(|| local_secrets.get("kadepay_db_user").cloned())
-            .expect("Missing KADEPAY_DB_USER environment variable or kadepay_db_user in secrets");
-        let db_password = env::var("KADEPAY_DB_PASSWORD")
+            .or_else(|| local_secrets.get(Self::KADEPAY_DB_USER_KEY).cloned())
+            .expect("Missing KADEPAY_DB_USER in environment variables or secrets");
+        let db_password = env::var(Self::KADEPAY_DB_PASSWORD_KEY)
             .ok()
-            .or_else(|| local_secrets.get("kadepay_db_password").cloned())
-            .expect("Missing KADEPAY_DB_PASSWORD environment variable or kadepay_db_password in secrets");
-        let db_name = env::var("KADEPAY_DB_NAME")
+            .or_else(|| local_secrets.get(Self::KADEPAY_DB_PASSWORD_KEY).cloned())
+            .expect("Missing KADEPAY_DB_PASSWORD in environment variables or secrets");
+        let db_name = env::var(Self::KADEPAY_DB_NAME_KEY)
             .ok()
-            .or_else(|| local_secrets.get("kadepay_db_name").cloned())
-            .expect("Missing KADEPAY_DB_NAME environment variable or kadepay_db_name in secrets");
+            .or_else(|| local_secrets.get(Self::KADEPAY_DB_NAME_KEY).cloned())
+            .expect("Missing KADEPAY_DB_NAME in environment variables or secrets");
 
         Config {
             kadepay_invoices_server_addr,
